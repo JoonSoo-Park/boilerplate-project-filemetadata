c5fa595319c07b6c681e616cef3c7f537b2bbb40
repl.it user <replituser@example.com>
2021-01-20 01:53:48 +0000

    1

--- server.js
+++ server.js
@@ -1,20 +1,32 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var cors = require('cors');
+var multer = require('multer');
+
 require('dotenv').config()
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', multer().single('upfile'), (req, res) => {
+  let resposeObject = {}
+  resposeObject['name'] = req.file.originalname;
+  resposeObject['type'] = req.file.mimetype;
+  resposeObject['size'] = req.file.size;
 
-
+  console.log(req.file);
+  res.json(resposeObject);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
